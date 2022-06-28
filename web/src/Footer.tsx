405005b0ade2a9a6a9a703d44068f817f483647d
thomas <sketchbang@gmail.com>
2022-06-28 07:02:24 -0500

    update footer

--- web/src/Footer.tsx
+++ web/src/Footer.tsx
@@ -5,14 +5,16 @@ export function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          Aptos <strong>Coin Emporium</strong> by Thomas Ruble & Ultima Labs.
-          The source code is
+          Aptos <strong>Coin Emporium</strong> by thomas@ultimalabs.xyz. The
+          source code is
           <a href="https://github.com/ultima-fi/aptos-faucet" target="_blank">
             &nbsp;here
           </a>
         </p>
 
-        <p>Program account: {FAUCET_MODULE}</p>
+        <p style={{ overflowWrap: "anywhere" }}>
+          Program account: {FAUCET_MODULE}
+        </p>
       </div>
     </footer>
   );
